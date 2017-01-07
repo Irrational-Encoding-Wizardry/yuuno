3dd815059adcd1587e7591aee69eddf82b1ab923
StuxCrystal <stuxcrystal@encode.moe>
2017-01-07 05:11:58 +0100

    Make Image-Reading more stable

--- yuuno/vsimg.py
+++ yuuno/vsimg.py
@@ -1,3 +1,4 @@
+import base64
 import ctypes
 import functools
 import itertools
@@ -8,12 +9,17 @@ from PIL import Image
 from yuuno.vendor import mvsfunc
 
 
+def get_bytelink(data, format="image/png"):
+    return "data:%s;base64,%s"%(format, base64.b64encode(data).decode())
+
+
 def get_plane(frame, planeno):
     width, height = frame.width, frame.height
-    buffer = (ctypes.c_char*(width*height))()
-    ctypes.memmove(buffer, frame.get_read_ptr(planeno), width*height)
-    return Image.frombuffer('L', (width, height), buffer, "raw", "L", width, 1)
-
+    ary = frame.get_read_array(planeno)
+    buffer = bytearray(len(ary)*len(ary[0]))
+    for stride_no, stride in enumerate(ary):
+        buffer[stride_no*len(stride):(stride_no+1)*len(stride)] = stride
+    return Image.frombuffer('L', (width, height), buffer, "raw", "L", len(stride), 1)
 
 def frame2image(clip, frameno=0):
     """
@@ -21,9 +27,9 @@ def frame2image(clip, frameno=0):
     """
     core = vapoursynth.get_core()
 
-    # Convert clip to RGB
     if clip.format.color_family != vapoursynth.RGB:
-        clip = mvsfunc.ToRGB(clip, compat=1)
+        clip = mvsfunc.ToRGB(clip)
+    clip = mvsfunc.Depth(clip, 8)
     
     # Extract metadata from the clip.
     width, height = clip.width, clip.height
@@ -32,14 +38,17 @@ def frame2image(clip, frameno=0):
     # Extract the frame.
     frame = clip.get_frame(frameno)
 
-    planesize = width*height*colorformat.bytes_per_sample
-    buffer = (ctypes.c_char*planesize)()
-    ctypes.memmove(buffer, frame.get_read_ptr(0), planesize)
+    planes = tuple(map(
+        functools.partial(get_plane, frame),
+        range(colorformat.num_planes)
+    ))
 
-    # Parse the PIL image
-    return Image.frombuffer('RGB', (width, height), buffer, "raw", "BGRX", frame.get_stride(0), -1)
+    return Image.merge("RGB", planes)
+
 
 def image2bytes(im):
     f = BytesIO()
+    if im.mode not in ("RGB", "1", "L", "P"):
+        im = im.convert("RGB")
     im.save(f, format="png")
     return f.getvalue()
