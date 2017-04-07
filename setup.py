04247578e95dddf87e4493074fbed140020ed36a
StuxCrystal <stuxcrystal@encode.moe>
2017-04-07 20:11:45 +0200

    Yes. A development branch will be awesome.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "yuuno",
-    version = "0.3.4.1",
+    version = "0.3.4.2",
     author = "StuxCrystal",
     author_email = "stuxcrystal@encode.moe",
     description = "Glue for jupyter and vapoursynth",
--- yuuno/__init__.py
+++ yuuno/__init__.py
@@ -7,6 +7,6 @@ def load_ipython_extension(ipython):
     install.initialize(ipython)
 
 
-version = (0, 3, "4")
+version = (0, 3, 4, 2)
 __version__ = ".".join(str(n) for n in version)
 __all__ = ["install", "diff", "compare", "inspect", "preview", "dump", "interact"]
