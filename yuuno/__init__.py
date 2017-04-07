8ad904896562b558598e510495d043916f62ff49
StuxCrystal <stuxcrystal@encode.moe>
2017-04-07 20:10:38 +0200

    Lol.

--- yuuno/__init__.py
+++ yuuno/__init__.py
@@ -2,10 +2,11 @@ from yuuno.features import install
 from yuuno.widgets.applications import diff, compare, inspect, preview, dump, interact
 from yuuno.settings import settings as _settings
 
+
 def load_ipython_extension(ipython):
     install.initialize(ipython)
 
 
-version = (0, 4, "0a0", "dev1")
+version = (0, 3, "4")
 __version__ = ".".join(str(n) for n in version)
 __all__ = ["install", "diff", "compare", "inspect", "preview", "dump", "interact"]
