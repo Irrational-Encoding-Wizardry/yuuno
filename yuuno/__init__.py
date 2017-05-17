bb2d3908c7656ab41a10a3cf3cc201627826b92f
Roland Netzsch <stuxcrystal@users.noreply.github.com>
2017-05-17 17:52:49 +0200

    Update Version Number pt1

--- yuuno/__init__.py
+++ yuuno/__init__.py
@@ -9,6 +9,6 @@ def load_ipython_extension(ipython):
     install.initialize(ipython)
 
 
-version = (0, 3, 4, 2)
+version = (0, 4, 0)
 __version__ = ".".join(str(n) for n in version)
 __all__ = ["install", "diff", "compare", "inspect", "preview", "dump", "interact"]
