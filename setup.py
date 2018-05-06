80d5a697456f5e72ae95a16496e481d27b599a5c
stuxcrystal <stuxcrystal@encode.moe>
2018-05-06 18:40:40 +0200

    It would be easy if we would use UTF-8 inside docker containers. pt2

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding="utf8") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.rst', encoding="utf8") as history_file:
     history = history_file.read()
 
 requirements = [
