7e3fd106a1e1e695626e37d26df40ec582b4b551
stuxcrystal <stuxcrystal@encode.moe>
2018-05-08 23:47:51 +0200

    Next dev version!

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ test_requirements = [
 
 setup(
     name='yuuno',
-    version='1.0.0.dev1',
+    version='1.0.0.dev2',
     description="Yuuno = Jupyter + VapourSynth",
     long_description=readme + '\n\n' + history,
     author="stuxcrystal",
