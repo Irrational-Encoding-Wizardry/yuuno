066eedc3c0ed00b683aa5b962532cd2eca8f118d
Roland Netzsch <stuxcrystal@encode.moe>
2018-06-06 19:18:20 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from setuptools import setup, find_packages
 
 with open('README.rst', encoding="utf8") as readme_file:
