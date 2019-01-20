bb1658b01e3c84649b13254044599d284413109f
Roland Netzsch <stuxcrystal@encode.moe>
2019-01-20 20:41:28 +0100

    Moved that stuff to yuuno-js

--- yuuno-jupyter/src/extension.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-Vue.config.devtools = true;
-Vue.use(Vuex);
-
-export {default as PreviewWindowWidget} from './widgets/PreviewWindow';
-export {default as EncodeWindowWidget} from './widgets/EncodeWindow';
